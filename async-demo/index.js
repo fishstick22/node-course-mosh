d287617da3a52b0adf2dfea09a836d67405c06b4
Jeff <jeff.burkholder@gmail.com>
2019-03-11 09:48:10 -0500

    async demo callback hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,19 @@
 console.log('Before');
-getUser(1, (user) => {
-    getRepositories(user, (repos) => {
-        console.log('Repos', repos);
-    })
-});
+getUser(1, getRepositories);
 
 console.log('After');
+
+function getRepositories(user) {
+    getRepositories(user.gitHubUserName, getCommits);
+}
+
+function getCommits(repos) {
+    getCommits(repo, displayCommits);
+}
+
+function displayCommits(commits) {
+    console.log('commits', commits);
+}
 
 function getUser(id, callback) {
     setTimeout(() => {
@@ -14,10 +22,16 @@ function getUser(id, callback) {
     }, 2000);
 }
 
-
 function getRepositories(username, callback) {
     setTimeout(() => {
         console.log('Calling GitHub API...');
-        callback(['repo1','repo2','repo3']);
+        callback(['repo1','repo2','repo3']);// some bug where callback is not a function
+    }, 2000);
+}
+
+function getCommits(repo, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        callback(['commit1','commit2','commit3']);
     }, 2000);
 }
