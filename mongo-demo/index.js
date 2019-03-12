0fb6c88bc0bb6eefcd6310d22d75074f1d83ee52
Jeff <jeff.burkholder@gmail.com>
2019-03-12 18:29:00 -0500

    mongo exercises

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -26,4 +26,28 @@ async function createCourse() {
     console.log('Document saved: ', result);
 }
 
-createCourse();
+async function getCourses() {
+    // eq
+    // ne
+    // gt
+    // gte
+    // lt
+    // lte
+    // in
+    // nin
+    const courses = await Course
+        // .find({ author: 'Mosh', isPublished: true } )
+        .find({ author: /^Mosh/i } )
+        // .find()
+        // .or([ {author: 'Mosh'}, {isPublished: true}])
+        // .and([ {author: 'Mosh'}, {isPublished: true}])
+        // .find({ price: { $gt: 10, $lte: 20 } } )
+        // .find({ price: { $in: [10, 15, 20] } } )
+        .limit(10)
+        .sort({ name: 1 })
+        // .select({ name: 1, tags: 1})
+        .count();
+    console.log('Courses', courses)
+}
+
+getCourses();
