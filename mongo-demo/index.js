03af1a3849bdd78722a7b758d81f50d6d21e4688
Jeff <jeff.burkholder@gmail.com>
2019-03-11 19:18:12 -0500

    mongo-demo

---
+++ mongo-demo/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(error => console.log('NOT Connected to MongoDB!!! ', error));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+    
+    const result = await course.save();
+    console.log('Document saved: ', result);
+}
+
+createCourse();
