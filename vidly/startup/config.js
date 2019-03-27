f224546b545ab0185d2dd88f6ea407f2a5075d84
fishstick22 <jeff.burkholder@gmail.com>
2019-03-26 20:21:39 -0500

    fixed error logger

--- vidly/startup/config.js
+++ vidly/startup/config.js
@@ -2,6 +2,6 @@ const config = require('config');
 
 module.exports = function() {
     if (!config.get('jwtPrivateKey')) {
-        throw new Errorthis ('FATAL ERROR: jwtPrivateKey is not defined.');
+        throw new Error ('FATAL ERROR: jwtPrivateKey is not defined.');
       }
 }
