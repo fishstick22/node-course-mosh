0db8e85f53e42f721dee45ceb5793d55fdffccc9
Jeff <jeff.burkholder@gmail.com>
2019-03-27 23:04:33 -0500

    testing exercise

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -19,101 +19,37 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", auth, async (req, res) => {
-  // const { error } = validateGenre(req.body);
-  // if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-  try {
-    await validate(req.body);
-  } catch (error) {
-    console.log(error.details[0].message);
-    return res.status(400).send(error.details[0].message);
-  }
-
-  try {
-    console.log("Passed validation");
-    const genre = new Genre({ name: req.body.name });
-
-    await genre.save();
-    res.send(genre);
-  } catch (error) {
-    console.log("caught error in save:", error);
-    return res.status(500).send(error);
-  }
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
+  
+  res.send(genre);
 });
 
-router.put("/:id", async (req, res) => {
-  try {
-    await validate(req.body);
-  } catch (error) {
-    console.log(error.details[0].message);
-    return res.status(400).send(error.details[0].message);
-  }
+router.put('/:id', [auth, validateObjectId], async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-  try {
-    //(node:9352) DeprecationWarning: collection.findAndModify is deprecated. Use findOneAndUpdate, findOneAndReplace or findOneAndDelete instead.
-    const id = req.params.id;
-    console.log("Put", id);
-    // const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
-    //   new: true
-    // });
-    // if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-    // res.send(genre);
-    await Genre.findOneAndUpdate(
-      { _id: id },
-      { $set: { name: req.body.name } },
-      { new: true }
-    ).exec((err, genre) => {
-      if (err) {
-        console.log(err.message);
-        if (err.name === "CastError" && err.kind === "ObjectId") {
-          return res
-            .status(404)
-            .json({ success: false, msg: "Genere id not found" });
-        }
-        return res.json({ success: false, msg: "Cannot update Genre" });
-      }
-      if (!genre) {
-        return res
-          .status(404)
-          .json({ success: false, msg: "Genere not updated" });
-      }
-      console.log("Updated", id);
-      res.send(genre);
-    });
-  } catch (error) {
-    res.status(500).send(error);
-    //return res.status(404).send('The genre with the given ID was not found.');
-  }
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+    new: true
+  });
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  
+  res.send(genre);
 });
 
-router.delete("/:id", [auth, admin], async (req, res) => {
-  try {
-    const id = req.params.id;
-    await Genre.findOneAndDelete({ _id: id }).exec(function(err, genre) {
-      if (err) {
-        return res
-          .status(404)
-          .json({ success: false, msg: "Genere id not found" });
-      }
-      console.log("Deleted", id);
-      res.json({ success: true, msg: "Genere deleted." });
-    });
-    //res.send(genre);
-  } catch (error) {
-    return res.status(404).send("The genre with the given ID was not found.");
-  }
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
 });
 
 router.get("/:id", validateObjectId, async (req, res) => {
-  // try {
-  //   const id = req.params.id;
-  //   const genre = await Genre.findById(id);
-  //   res.send(genre);
-  // } catch (error) {
-  //   console.log(error.message);
-  //   return res.status(404).send('The genre with the given ID was not found.');
-
-  // }
 
   const genre = await Genre.findById(req.params.id);
 
