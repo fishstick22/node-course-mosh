36e219a309bef1542e154760a945e7cf9fba6558
Jeff <jeff.burkholder@gmail.com>
2019-03-28 19:41:18 -0500

    test driven development

--- vidly/models/rental.js
+++ vidly/models/rental.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
+const moment = require('moment');
 const mongoose = require('mongoose');
 
-const Rental = mongoose.model('Rental', new mongoose.Schema({
+const rentalSchema = new mongoose.Schema({
   customer: { 
     type: new mongoose.Schema({
       name: {
@@ -53,12 +54,26 @@ const Rental = mongoose.model('Rental', new mongoose.Schema({
     type: Number, 
     min: 0
   }
-}));
+});
+
+rentalSchema.statics.lookup = function(customerId, movieId) {
+  return this.findOne({
+    'customer._id': customerId,
+    'movie._id': movieId,
+  });
+}
+
+rentalSchema.methods.return = function() {
+  this.dateReturned = new Date();
+
+  const rentalDays = moment().diff(this.dateOut, 'days');
+  this.rentalFee = rentalDays * this.movie.dailyRentalRate;
+}
+
+const Rental = mongoose.model('Rental', rentalSchema);
 
 function validateRental(rental) {
   const schema = {
-    // customerId: Joi.string().required(),
-    // movieId: Joi.string().required()
     customerId: Joi.objectId().required(),
     movieId: Joi.objectId().required()
   };
